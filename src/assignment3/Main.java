93da497da27fc0e4c55a1a5c767f193eaa8e2ad8
michalspano <michal.spano03@gmail.com>
2022-10-08 21:28:47 +0200

    Add `Main.java` for testing purposes

---
+++ src/assignment3/Main.java
@@ -0,0 +1,8 @@
+package assignment3;
+
+public class Main {
+    // currently, the main shall be used strictly for testing purposes;
+    public static void main(String[] args) {
+        System.out.println("Assignment 3\nTesting...");
+    }
+}
