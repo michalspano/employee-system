9c1f83b0f9055f2dc5a92bcbf33b7d741d76b115
ionelpop <ionelalexandru.pop@gmail.com>
2022-10-13 19:49:53 +0200

    Delete Main.java

--- src/assignment3/Main.java
+++
@@ -1,8 +0,0 @@
-package assignment3;
-
-public class Main {
-    // currently, the main shall be used strictly for testing purposes;
-    public static void main(String[] args) {
-        System.out.println("Assignment 3\nTesting...");
-    }
-}
