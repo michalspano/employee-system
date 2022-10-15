a9194fa55f1c777d01fc2e92a90131c1cba86d98
michalspano <michal.spano03@gmail.com>
2022-10-15 16:57:13 +0200

    Polish `CustomExceptions`

--- src/assignment3/CustomExceptions/StringCannotBeEmptyException.java
+++ src/assignment3/CustomExceptions/StringCannotBeEmptyException.java
@@ -1,22 +1,21 @@
 /***************************************************************************************************
  * Group Work Assignment 3 - A3-Group 20
- * File: StringCannotBeEmpty.java
+ * File: CustomExceptions/StringCannotBeEmpty.java
  * Members: Ionel Pop, Joel Mattsson, Michal Spano
  * For DIT043: Object Oriented Programming; SEM@GU.
  ***************************************************************************************************/
 
 package assignment3.CustomExceptions;
 
-public class StringCannotBeEmptyException extends Exception {
-        
+public class StringCannotBeEmptyException extends Exception 
+{        
     public StringCannotBeEmptyException()
     {
-        // default Exception message
-        super("String cannot be empty.");
+        super("String cannot be empty."); // default message
     }
 
     public StringCannotBeEmptyException(String message)
     {
-        super(message);
+        super(message); // user-defined custom message
     }
 }
